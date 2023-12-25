5e6e602331bb2d0b2a6c9393487d172031d8c3b9
Vishal Gupta <63388692+vishalguptax@users.noreply.github.com>
2023-12-25 19:46:19 +0530

    Update NiftyStats.tsx

--- components/shared/nifty-stats/NiftyStats.tsx
+++ components/shared/nifty-stats/NiftyStats.tsx
@@ -1,23 +1,3 @@
 export const NiftyStats = async () => {
-  const url =
-    "https://latest-stock-price.p.rapidapi.com/price?Indices=NIFTY%2050&Identifier=NIFTY%2050";
-  const options = {
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Key": "ea1a025551mshd933f1c89a2f96ap1f8855jsn675752bf9bc1",
-      "X-RapidAPI-Host": "latest-stock-price.p.rapidapi.com",
-    },
-  };
-
-  const Comp = (data: any) => (
-    <div style={{ color: "red" }}>{data?.data[0]?.lastPrice}</div>
-  );
-
-  try {
-    const response = await fetch(url, options);
-    const result = await response.json();
-    return <Comp data={result} />;
-  } catch (error) {
-    console.error(error);
-  }
+ 
 };
