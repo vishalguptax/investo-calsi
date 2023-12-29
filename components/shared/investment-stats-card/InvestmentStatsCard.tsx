397f470b8d0ad15b3609c37a0330189568b94d3b
Vishal Gupta <vishal.jsx@gmail.com>
2023-12-30 03:08:43 +0530

    feat: number counter added

--- components/shared/investment-stats-card/InvestmentStatsCard.tsx
+++ components/shared/investment-stats-card/InvestmentStatsCard.tsx
@@ -1,18 +1,29 @@
+import CountUp from "react-countup";
 import { Card } from "@/components/ui/card/Card";
 import { InvestmentStatsType } from "../form-container/FormContainer";
 import { formatNumber } from "@/utils/formatNumber";
 import styles from "./investmentStatsCard.module.css";
+
 export const InvestmentStatsCard = (data: InvestmentStatsType) => {
   return (
     <Card className={styles.stat_card}>
       <div className={styles.stat_container}>
-        <p>Total Returns</p> <p>₹ {formatNumber(data.totalReturns)}</p>
+        <p>Total Returns</p>{" "}
+        <p>
+          ₹ <CountUp end={data.totalReturns} duration={1} />
+        </p>
       </div>
       <div className={styles.stat_container}>
-        <p>Total Investment</p> <p>₹ {formatNumber(data.totalInvestment)}</p>
+        <p>Total Investment</p>{" "}
+        <p>
+          ₹ <CountUp end={data.totalInvestment} duration={1} />
+        </p>
       </div>
       <div className={styles.stat_container}>
-        <p>Total Value</p> <p>₹ {formatNumber(data.totalValue)}</p>
+        <p>Total Value</p>{" "}
+        <p>
+          ₹ <CountUp end={data.totalValue} duration={1} />
+        </p>
       </div>
     </Card>
   );
